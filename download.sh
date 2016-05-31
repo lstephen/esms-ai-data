3d2ce28d687b0603aedf41688d997d55323c5dfd
lstephen <levi.stephen@gmail.com>
2016-05-31 10:53:22 -0700

    Add download script. Update ESL/UCFL rosters

---
+++ download.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+mkdir tmp
+
+echo "Downloading WAT roster..."
+
+curl -L http://www.eslmanager.com/England/Premiership/Rosters/wat.txt -o rosters/Esl/wat.txt
+
+echo "WAT Done."
+
+echo "Downloading GLI roster..."
+
+pushd tmp
+
+curl -L http://www.ucfl.co.uk/rosters/senior.zip -o senior.zip
+curl -L http://www.ucfl.co.uk/rosters/youth.zip -o youth.zip
+
+unzip -o senior.zip
+unzip -o youth.zip
+
+popd
+
+cp tmp/div2/gli.txt rosters/Ucfl/gli.txt
+cp tmp/youthdiv2/gly.txt rosters/Ucfl/gly.txt
+
+#rm -rf tmp
