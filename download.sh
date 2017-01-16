881f857b6a614a157dcb927265559d9336330b66
lstephen <levi.stephen@gmail.com>
2017-01-16 15:43:44 +0200

    Update download script
    Also, Esl, Ssl, Ucfl rosters

--- download.sh
+++ download.sh
@@ -4,6 +4,13 @@ set -e
 
 mkdir tmp
 
+echo "Downloading MIS roster..."
+
+curl -L http://www.ssl2001.ukhome.net/mis.txt -o rosters/Ssl/mis.txt
+curl -L http://www.ssl2001.ukhome.net/msy.txt -o rosters/Ssl/msy.txt
+
+echo "MIS Done."
+
 echo "Downloading WAT roster..."
 
 curl -L http://www.eslmanager.com/England/Premiership/Rosters/wat.txt -o rosters/Esl/wat.txt
