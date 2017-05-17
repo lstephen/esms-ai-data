a44dc22ae84ae4b9e9cea0600ab030b4fce4f9df
lstephen <levi.stephen@gmail.com>
2017-05-17 12:45:14 -0700

    update downlad.sh to include SSL FAs

--- download.sh
+++ download.sh
@@ -8,6 +8,7 @@ echo "Downloading MIS roster..."
 
 curl -L http://www.ssl2001.ukhome.net/mis.txt -o rosters/Ssl/mis.txt
 curl -L http://www.ssl2001.ukhome.net/msy.txt -o rosters/Ssl/msy.txt
+curl -L http://www.ssl2001.ukhome.net/free_agents.txt -o rosters/Ssl/free_agents.txt
 
 echo "MIS Done."
 
