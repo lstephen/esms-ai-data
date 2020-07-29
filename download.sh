0e550c120b44d9b1992aee7eebc638b0f6a97be1
lstephen <levi.stephen@gmail.com>
2020-07-29 09:11:17 -0700

    Update rosters

--- download.sh
+++ download.sh
@@ -12,7 +12,7 @@ echo "MIS Done."
 
 echo "Downloading WBA roster..."
 
-curl -L http://eslmanager.co.uk/rosters/wba.txt -o rosters/Esl/wba.txt
+curl -L http://eslmanager.co.uk/rosters/ECH/wba.txt -o rosters/Esl/wba.txt
 
 echo "WBA Done."
 
